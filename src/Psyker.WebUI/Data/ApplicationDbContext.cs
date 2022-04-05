9d60d6773ce81659ff28cf89df5a6c496b3d7cbc
Michal Gajda <michal@gajda.co.uk>
2022-04-05 18:48:28 +0100

    Init

---
+++ src/Psyker.WebUI/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Psyker.WebUI.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ src/Psyker.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Psyker.WebUI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
